6be7bb98cd58f7edcd668b597f42ee29710d7175
Huanianss <40359763+Huanianss@users.noreply.github.com>
2023-06-07 16:44:44 +0800

    Update example02.py

--- example02.py
+++ example02.py
@@ -19,10 +19,4 @@ print(c0, c.double())
 print(type(c))
 print('len: ', len(c))
 
-# import torch
-# a=torch.rand([4,5])
-# torch.Tensor
-# print(a.dtype)
-# # a=a.numpy()
-# b=np.int8(a)
-# print(b)
+ 
